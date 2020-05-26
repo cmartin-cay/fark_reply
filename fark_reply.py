4501f1e3c7b5f5916405b3a1f1c2cca6407b7518
Colin Martin <colin.martin@gmail.com>
2020-05-26 13:12:31 -0500

    Special response for Florida and NewsFlash tags

--- fark_reply.py
+++ fark_reply.py
@@ -19,7 +19,7 @@ search_url = "https://www.fark.com/hlsearch"
 fark_user_id = "14804898"
 vulgar_user_id = "740888554008543232"
 response_pool = [
-    "Come argue with farkers in the comment thread",
+    # "Come argue with farkers in the comment thread",
     "Never leave your basement again with the help of comment thread",
     "It's now news, it's the comment thread",
     "Sure the headline is great, but the real fun is in the comment thread",
@@ -39,11 +39,11 @@ response_pool = [
     "Surely nobody could have a problem with this....right",
     "If whiskey is the water of life, then surely... well Fark is alright too I guess",
     "If you were promised that there would be no math, there probably won't be",
-    "If you're reading this on the toilet, we know who you are, and what you've done",
+    # "If you're reading this on the toilet, we know who you are, and what you've done",
     "The longer you wait, the harder it gets",
     "In every thread in every tab on this site, there is a nobody who dreams of being a somebody. Or at least getting a few funny votes",
     "Work sucks. But farking at work is awesome",
-    "Feel the rythym, feel the rhyme, get on up, it's commenting time",
+    # "Feel the rythym, feel the rhyme, get on up, it's commenting time",
     "You don't get to 500 comments without making a few enemies",
     "Fark. Where we've been practicing social distancing since before it was popular",
     "This funny comment thread removed for trolling",
@@ -61,122 +61,47 @@ response_pool = [
 ]
 
 
-def unshortern_url(shortened_url: str) -> str:
-    pass
-
-
-def create_payload(search_term: str) -> dict:
-    """
-    Creates a payload item for use in a reauest object
-    :param search_term: A string generated from a twitter status
-    :return: A dictionary formatted for the Fark search tool
-    """
-    return {"qq": search_term, "o": 0}
-
-
-def get_relevancy_score(soup: BeautifulSoup) -> int:
-    """
-    Searches the Fark soup and return the relevancy score of the first result
-    :param soup: Fark website converted to BS4 soup
-    :return: A relevancy score
-    """
-    relevency = soup.find("span", attrs={"style": {"font-size:smaller"}})
-    # returns "(score xxx%)"
-    # so slice the string to just get the % then convert to int
-    return int(relevency.text[7:-2])
-
-
 def create_tweet_reply(soup: BeautifulSoup) -> str:
-    fark_tag = [a["title"] for a in soup.select(".headlineTopic a")]
+    fark_tag = [a["title"] for a in soup.select(".commentHeadlineContainerTopic a")]
     if fark_tag[0] == "Florida":
         return "Florida Man thread"
+    elif fark_tag[0] == "NewsFlash":
+        return "This is a NewsFlash. Please be mindful of that in the thread"
     else:
         return random.choice(response_pool)
 
 
-def make_fark_soup(search_term: str) -> BeautifulSoup:
+def make_fark_soup(fark_url: str) -> BeautifulSoup:
     """
-
-    :param search_term: The string you want to search in the Fark search engine
+    :param fark_url: The url to the comments thread
     :return: BeautifulSoup object of the page returned by the Fark search engine
     """
-    if not search_term:
-        return None
-    r = requests.get(search_url, create_payload(search_term))
+
+    r = requests.get(fark_url)
     soup = BeautifulSoup(r.text, features="html.parser")
     return soup
 
 
-def get_fark_comment_link(soup: BeautifulSoup) -> str:
-    """
-    Parses the fark website soup to return the url of the first comment thread
-    :param soup: Fark website converted to BS4 soup
-    :return: URL of the first comment thread
-    """
-    # Check to see if any results were found
-    comment_threads = soup.findAll("div", {"class": "icon_comment_container"})
-    if len(comment_threads) == 0:
-        return None
-    # Once we know there are results, identify all the links to the comment threads
-    comment_thread_links = [
-        a["href"] for a in soup.select(".icon_comment_container a[href]")
-    ]
-    return comment_thread_links[0]
-
-
-def get_fark_response(search_term):
-    if not search_term:
-        return None
-    r = requests.get(search_url, create_payload(search_term))
-    soup = BeautifulSoup(r.text, features="html.parser")
-    mydivs = soup.findAll("div", {"class": "icon_comment_container"})
-    if len(mydivs) == 0:
-        return None
-    if get_relevancy_score(soup) < 75:
-        print(f"Relevancy score: <75%")
-        return None
-    fark_tag = [a["title"] for a in soup.select(".headlineTopic a")]
-    tweet_reply_text = create_tweet_reply(fark_tag[0])
-    res = [a["href"] for a in soup.select(".icon_comment_container a[href]")]
-    return (res[0], tweet_reply_text)
-
-
-# Set up the Tweepy API
 def authorize_tweepy(consumer_key, consumer_secret, access_token, access_secret):
+    """
+    Sign in to Twitter and gain access to the API
+    :param consumer_key:
+    :param consumer_secret:
+    :param access_token:
+    :param access_secret:
+    :return:
+    """
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
     return api
 
 
-def tweet_text(status, text):
-    """
-    Take a tweet object, and the already established text of the tweet and returns
-    None or the cleaned text
-    :param status: Twitter status object
-    :param text: Previously established text of the tweet
-    :return: None or Cleaned text suitable for passing to the Fark search page
-    """
-    if (
-        status.in_reply_to_status_id
-        or text.startswith("RT @")
-        or hasattr(status, "quoted_status")
-    ):
-        print(text)
-        print("Probably a reply or a retweet")
-        print("______________________________")
-        return None
-    # Strip out http references at the end of the tweet
-    # Unfortunately this will remove all http links - this can be worked on
-    text = re.sub(r"http\S+", "", text).rstrip()
-    return text
-
-
 def valid_tweet(status):
     if (
-        status.in_reply_to_status_id
-        or status.text.startswith("RT @")
-        or hasattr(status, "quoted_status")
+            status.in_reply_to_status_id
+            or status.text.startswith("RT @")
+            or hasattr(status, "quoted_status")
     ):
         return False
     else:
@@ -197,8 +122,9 @@ class MyStreamListener(tweepy.StreamListener):
         tweet_id = status.id
         # Step 1: Identify if a tweet is a fark link or not
         if valid_tweet(status):
+            # TODO IndexError list index out of range
             if "extended_tweet" in status._json:
-                url = status.extended_tweet['entities']['urls'][0]['expanded_url']
+                url = status.extended_tweet["entities"]["urls"][0]["expanded_url"]
             else:
                 url = status.entities["urls"][0]["expanded_url"]
             print(url)
@@ -207,7 +133,8 @@ class MyStreamListener(tweepy.StreamListener):
             return
 
         # Step 2: Post the response
-        fark_response = random.choice(response_pool)
+        soup = make_fark_soup(url)
+        fark_response = create_tweet_reply(soup)
         fark_response = f"@fark {fark_response} {fark_url}"
         print(fark_response)
         api.update_status(status=fark_response, in_reply_to_status_id=tweet_id)
